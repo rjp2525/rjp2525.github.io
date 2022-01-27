287153627a36ce0e93e00541ee3413cc702ca143
rjp2525 <renophilibert@gmail.com>
2022-01-26 19:30:53 -0500

    Added fade animate-on-scroll (AOS)

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <Header />
-    <About />
-    <Skills />
-    <Modal />
-    <Projects />
-    <Contact />
+    <Header data-aos="fade" data-aos-duration="800" />
+    <About data-aos="fade-up" data-aos-duration="800" />
+    <Skills data-aos="fade-up" data-aos-duration="800" />
+    <Modal data-aos="fade-up" data-aos-duration="800" />
+    <Projects data-aos="fade-up" data-aos-duration="800" />
+    <Contact data-aos="fade-up" data-aos-duration="800" />
     <Footer />
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import AOS from 'aos'
 import Header from '@/components/Sections/Header'
 import About from '@/components/Sections/About'
 import Skills from '@/components/Sections/Skills'
@@ -19,6 +20,9 @@ import Projects from '@/components/Sections/Projects'
 import Modal from '@/components/Modals/Modal'
 import Contact from '@/components/Sections/Contact'
 import Footer from '@/components/Sections/Footer'
+
+// AOS CSS
+import 'aos/dist/aos.css'
 
 export default {
   name: 'IndexPage',
@@ -31,6 +35,10 @@ export default {
     Modal,
     Contact,
     Footer,
+  },
+
+  created() {
+    AOS.init()
   },
 
   mounted() {
